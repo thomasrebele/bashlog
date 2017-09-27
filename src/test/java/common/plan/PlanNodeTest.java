a29fc0b7a378cb1d970d8da22efbaf142d327e55
Tpt <thomaspt@hotmail.fr>
2017-09-27 18:25:16 +0200

    Improves UnionNode simplification

--- src/test/java/common/plan/PlanNodeTest.java
+++ src/test/java/common/plan/PlanNodeTest.java
@@ -8,7 +8,11 @@ public class PlanNodeTest {
   @Test
   public void testSimplify() {
     Assert.assertEquals(
-            new UnionNode(new TableNode("foo")),
+            new UnionNode(new TableNode("foo"), new TableNode("bar")),
+            new UnionNode(new TableNode("foo"), new UnionNode(new TableNode("bar"), new UnionNode())).simplify()
+    );
+    Assert.assertEquals(
+            new TableNode("foo"),
             new UnionNode(new TableNode("foo"), new UnionNode(new TableNode("foo"), new UnionNode())).simplify()
     );
   }
