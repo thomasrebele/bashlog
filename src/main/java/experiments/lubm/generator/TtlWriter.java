8dbb87ef8e667e162ae42e5ed59fdc1d0a367e3e
Thomas Rebele <thomas.rebele@googlemail.com>
2017-10-27 10:53:44 +0200

    fixing prefix of sparql queries

--- src/main/java/experiments/lubm/generator/TtlWriter.java
+++ src/main/java/experiments/lubm/generator/TtlWriter.java
@@ -18,9 +18,9 @@ public class TtlWriter implements Writer {
     new File(path).mkdirs();
     this.path = path;
     writer = new PrintWriter(path + "all.ttl");
-    writer.write("@base <http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#> .\n");
+    writer.write("@base <http://swat.cse.lehigh.edu/onto/univ-bench.owl#> .\n");
     writer.write("@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>.\n");
-    writer.write("@prefix ub: <http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>.\n");
+    writer.write("@prefix ub: <http://swat.cse.lehigh.edu/onto/univ-bench.owl#>.\n");
   }
 
   @Override
